9c6e5da978d329cdf4b5e36fec64f3e50f2b7789
digsbyt1 <tdigsby@gmail.com>
2015-02-16 14:50:08 -0500

    add server socket and thread

--- FTserver.java
+++ FTserver.java
@@ -1,4 +1,26 @@
+import java.net.*;
+import java.io.*;
 public class FTserver
 {
-	/*this is the beginning of server file 
+	/*this is the beginning of server file */
+	public static void main(String[] args)
+	{
+		int counter = 0;
+		try
+		{
+			ServerSocket socket = new ServerSocket(1234);
+			while(true)
+			{
+				System.out.println("first mode");
+				Socket connectSocket = socket.accept();
+				counter++;
+				FTthread thread = new FTthread(counter, connectSocket);
+				thread.start();
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+} 
 }
